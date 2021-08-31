419a5e39c6d4139527ad33a33ed86a0761c7dcdf
Administrator <Administrator@DESKTOP-8F6DJTS>
2021-08-31 14:45:35 +0700

    GiaiPT

---
+++ n3108/n3108/Controllers/GiaiPTController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace n3108.Controllers
+{
+    public class GiaiPTController : Controller
+    {
+        GiaiPT gpt = new GiaiPT();
+        // GET: GiaiPT
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TimNghiem(string soA, string soB)
+        {
+            double soA1 = double.Parse(soA);
+            double soB1 = double.Parse(soB);
+            ViewBag.nghiempt = gpt.GiaiPhuongTrinhBacNhat(soA1, soB1);
+            return View();
+        }
+    }
+}
---
+++ n3108/n3108/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="n3108.MvcApplication" Language="C#" %>
